348b826af64ac0b18a030629c71fcbc67155aded
Benoît Suttor <bsuttor@cirb.irisnet.be>
2011-11-07 15:48:14 +0100

    add img folder

--- cirbpolicy/novac/setuphandlers.py
+++ cirbpolicy/novac/setuphandlers.py
@@ -2,6 +2,11 @@
 from zope.interface import alsoProvides
 from Products.CMFPlone.interfaces import IHideFromBreadcrumbs
 from cirb.novac.browser.novacview import INovacView
+import os
+
+def get_package_path():
+    from cirb.novac import config
+    return os.path.dirname(config.__file__)
 
 def setupNovac(context):
 
@@ -22,14 +27,29 @@ def setupNovac(context):
         alsoProvides(novac, INovacView)
         portal_workflow.doActionFor(novac,'publish')
         
-        novac_nl = site.invokeFactory(type_name='Folder', id=NOVACNL)
+        site.invokeFactory(type_name='Folder', 
+                                      id=NOVACNL,
+                                      title="NL Permis d'urbanisme",
+                                      description="",
+                                      language="nl")
         novac_nl = getattr(site, NOVACNL)
-        novac_nl.setTitle("Page novac_nl")
-        novac_nl.setDescription("")
-        novac_nl.setLanguage("nl")
         alsoProvides(novac_nl, INovacView)
         portal_workflow.doActionFor(novac_nl,'publish')
         novac_nl.addTranslationReference(novac)
+        
+        novac.invokeFactory(type_name='Folder', id='img', title='img',description="", excludeFromNav=True)
+        img = novac.img
+        portal_workflow.doActionFor(img,'publish')
+        #alsoProvides(img, IHideFromBreadcrumbs)
+        
+        from OFS.Image import File
+        path = os.sep.join([get_package_path(),"img"])
+        dir_list = os.listdir(path)
+        for filename in dir_list:
+            imgfile = File("dummy", "dummy", open('%s/%s' % (path,filename),"rb"))
+            img.invokeFactory(type_name='Image', id=filename)
+            my_img = getattr(img,filename)
+            my_img.setImage(imgfile)
         
         logger = context.getLogger("Novac")
         logger.info('end install Novac')
