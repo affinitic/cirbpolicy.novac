ba0ef08bab8d58bf598aa9995220af9cc8a62a82
Benoît Suttor <bsuttor@cirb.irisnet.be>
2013-03-07 09:19:44 +0100

    Preparing release 1.4.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of cirb.novacwaws
 import os
 from setuptools import setup, find_packages
 
-version = '1.5.dev0'
+version = '1.4.1'
 
 tests_require = ['zope.testing']
 
