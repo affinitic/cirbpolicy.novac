6f46aec2c180706a6e8fa4174ff555f4999e3f24
Catherine Corbesier <ccorbesier@cirb.irisnet.be>
2014-04-22 12:33:55 +0200

    Preparing release 1.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of cirb.novacwaws
 import os
 from setuptools import setup, find_packages
 
-version = '1.4.2.dev0'
+version = '1.4.2'
 
 tests_require = ['zope.testing']
 
